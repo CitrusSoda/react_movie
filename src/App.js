240dd1c3bd3828291c46c77069f549fcfc0bb254
citrussoda <dndhkaus1004@naver.com>
2019-11-13 23:10:13 +0900

    Add: set base movie component

--- src/App.js
+++ src/App.js
@@ -4,27 +4,23 @@ import PropTypes from "prop-types";
 
 //클래스로 할려면 필수
 class App extends React.Component {
-    //state에 바꾸고싶은 data를 넣는다
+    //state에 안넣어도 돌아감
     state = {
-        count: 0
-    };
-    // setState를 사용하지 않으면 render함수를 부를 수 없다
-    add = () => {
-        // 추천하는 방법
-        this.setState(current => ({count: current.count + 1}));
-    };
-    minus = () => {
-        // this.state는 성능저하 문제 발생가능성이 있다.
-        this.setState({count: this.state.count - 1});
+        isLoading: true,
+        movies: []
     };
 
-    // react.component에있는 함수
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({isLoading: false})
+        }, 6000);
+    }
+
     render() {
+        const {isLoading} = this.state;
         return (
             <div>
-                <h1>The number is : {this.state.count}</h1>
-                <button onClick={this.add}>Add</button>
-                <button onClick={this.minus}>Minus</button>
+                {isLoading ? "Loading..." : "We are ready"}
             </div>
         )
     }
