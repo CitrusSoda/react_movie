310ae46abbfcc5e5414e5d3420763064efb70e6d
citrussoda <dndhkaus1004@naver.com>
2019-11-13 22:32:30 +0900

    Learned: JSX&Props + propTypes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from 'react';
+//prop 확인
+import PropTypes from "prop-types";
+
+//{a} = props.a
+function Food({name, picture, rating}) {
+    return (
+        <div>
+            <h1>I like {name}</h1>
+            <h4>rating is {rating}</h4>
+            <img src={picture} alt={name}/>
+        </div>
+    );
+}
+
+const foodILike = [
+    {
+        id: 1,
+        name: "Kimchi",
+        image: "https://cdn.pixabay.com/photo/2017/06/10/19/32/kimchi-2390565_640.jpg",
+        rating: 4.9,
+    }
+];
+
+Food.propTypes = {
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired
+}
 
 function App() {
     return (
-        <div>Hello</div>
+        <div>
+            {foodILike.map(dish => (
+                <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+            ))}
+        </div>
     );
 }
 
